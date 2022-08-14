891ebdc6127ed7191eb32dec98eba75e48a80f3f
kalywann <ivankalytuk23@gmail.com>
2022-08-15 00:23:06 +0300

    button bug fix

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let burger = document.getElementById('burger')
 let nav = document.getElementById('nav')
+let body = document.getElementById('body')
+
 
 burger.onclick = function(){
     console.log('burger mobile')
     nav.classList.toggle('_active')
-    
+    body.classList.toggle('_lock')
 }
