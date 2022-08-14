b02ebec73d4f7b67566ce9e21b0b49d9746422f7
kalywann <ivankalytuk23@gmail.com>
2022-08-15 01:56:59 +0300

    cat not changes

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
+// burger menu
 let burger = document.getElementById('burger')
 let nav = document.getElementById('nav')
 let body = document.getElementById('body')
-
 
 burger.onclick = function(){
     console.log('burger mobile')
     nav.classList.toggle('_active')
     body.classList.toggle('_lock')
+}
+
+let button = document.getElementById('slidermob')
+let button_inner = document.getElementById('button_inner')
+
+
+button.onclick = function(){
+    console.log('change cat buttin')
+    button_inner.classList.toggle('_active')
+
 }
